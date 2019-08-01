fa6d2a7d9a7af0c8cf1bb39ccb13249398ae2771
stashat <stasa.tadic@gmail.com>
2019-08-02 00:13:39 +0200

    user access defined

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+# mongo ds149742.mlab.com:49742/heroku_776l5bh4 -u connection_user -p cakiproject123
+# mongodb://connection_user:cakiproject123@ds149742.mlab.com:49742/heroku_776l5bh4
+
+import sys
 import pymongo
+from pymongo import MongoClient
 
-client = pymongo.MongoClient("ds149742.mlab.com:49742/heroku_776l5bh4")
-db = client["connection_user"]
-books_col = db["books"]
-users_col = db["users"]
-
+connection = MongoClient("ds149742.mlab.com", 49742)
+db = connection["heroku_776l5bh4"]
+# MongoLab has user authentication
+db.authenticate("connection_user", "cakiproject123")
