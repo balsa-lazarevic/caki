6c7345f5c5dcc453623ceda13ae420a923e36c0d
stashat <stasa.tadic@gmail.com>
2019-08-02 19:13:13 +0200

    books validation

--- test.py
+++ test.py
@@ -2,6 +2,18 @@ import pymongo
 import jsonschema
 from jsonschema import validate
 
+MONGO_HOST = 'ds149742.mlab.com'
+MONGO_PORT = 49742
+MONGO_DB   = 'heroku_776l5bh4'
+MONGO_USER = 'connection_user'
+MONGO_PASS = 'cakiproject123'
+BOOKS_COLL = 'books'
+USERS_COLL = 'users'
+
+connection = pymongo.MongoClient(host=MONGO_HOST, port=MONGO_PORT)
+db = connection[MONGO_DB]
+db.authenticate(MONGO_USER, MONGO_PASS)
+
 books_v = {
       "$jsonSchema": {
           "bsonType": "object",
@@ -48,3 +60,37 @@ books_v = {
           }
       }
     }
+
+
+# books_col = db.create_collection("books_test", validator=books_v)
+books_col = db["books_test"]
+
+# Svaki user je opisan sledecim atributima:
+#   name - String required
+#   year - Number required 
+
+try:
+    my_book_1 = { 
+        "name": "Staki",
+        "price": 20,
+        "description": "neki glupi opis",
+        "quantity": 7,
+        "pages": 1245,
+        "image": "staki.jpg"
+    }
+    doc1 = books_col.insert_one(my_book_1)
+except Exception as e:
+    print(e)
+
+try:
+    my_book_2 = { 
+        "name": "Aki",
+        "price": 2000,
+        "description": "neki glupi opis",
+        "quantity": 4,
+        "pages": 1245,
+        "image": "aki.jpg"
+    }
+    doc2 = books_col.insert_one(my_book_2)
+except Exception as e:
+    print(e)
