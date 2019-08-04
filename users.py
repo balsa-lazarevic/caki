bdaf5684b51df671054b2cea3499d881f1c4fdb9
stashat <stasa.tadic@gmail.com>
2019-08-04 20:41:30 +0200

    users.py validator

---
+++ users.py
@@ -0,0 +1,41 @@
+import pymongo
+import jsonschema
+from jsonschema import validate
+
+users_v = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": [ "username", "password", "email", "role" ],
+        "properties": {
+        # "user_id":  
+            "username": {
+                "bsonType": "string",
+                "description": "unique index, string from 3 to 20 characters",
+                "minLength": 3,
+                "maxLength": 20
+            },
+            "password": {
+                "bsonType": "string",
+                "description": "password is a string from 5 to 25 characters written as SHA256 hash of 64 HEX characters",
+                "minLength": 64,
+                "maxLength": 64
+            },
+            "email": {
+                "bsonType": "string",
+                "description": "string from 5 to 35 characters",
+                "minLength": 5,
+                "maxLength": 35
+            },
+            "role": {
+                "bsonType": "int",
+                "description": "integer number of boks as number from 1 to 10, default is 1",
+                "minimum": 0,
+                "maximum": 1
+            },
+            "book": {
+                "bsonType": "object",
+                "description": "list of book object IDs",
+            }
+        }
+    }
+}
