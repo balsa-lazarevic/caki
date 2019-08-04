c56916fff5fd73b09b76b2a41781a46d1f87c957
aidaIsm <aida.ismailisufi@gmail.com>
2019-08-04 14:01:47 +0200

    Heroku MongoDB connection

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import pymongo
 
-client = pymongo.MongoClient("ds149742.mlab.com:49742/heroku_776l5bh4")
-db = client["connection_user"]
+uri = 'mongodb://aida:aidacoinis96@ds149742.mlab.com:49742/heroku_776l5bh4'
+client = pymongo.MongoClient(uri)
+db = client.get_default_database()
 books_col = db["books"]
 users_col = db["users"]
+
+
+
+
+
 
